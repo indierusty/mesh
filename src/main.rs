3ea5fdeb872689f2bbe2a3edd4b37570fc21170c
indierusty <indierusty@gmail.com>
2025-06-14 19:31:57 +0530

    trying out kurbo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use kurbo::{BezPath, PathEl, Point};
 use macroquad::prelude::*;
 use mesh::{HEIGHT, WIDTH};
 
@@ -12,6 +13,20 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
+    let points = vec![(10., 10.), (10., 100.)]
+        .iter()
+        .enumerate()
+        .map(|(i, (x, y))| {
+            if i == 0 {
+                PathEl::MoveTo(Point::new(*x, *y))
+            } else {
+                PathEl::LineTo(Point::new(*x, *y))
+            }
+        })
+        .collect::<Vec<PathEl>>();
+
+    let bezpath = BezPath::from_vec(points);
+
     loop {
         clear_background(WHITE);
         next_frame().await

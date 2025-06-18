24ef605bfd6421a7cde04c052b04a9a52015af23
indierusty <indierusty@gmail.com>
2025-06-18 20:42:36 +0530

    impl method to append a point and segment to the mesh struct

--- src/mesh.rs
+++ src/mesh.rs
@@ -4,12 +4,9 @@ use kurbo::{BezPath, PathEl, Point};
 
 use crate::next_id::NextId;
 
-pub type PointId = usize;
-pub type SegmentId = (PointId, PointId);
-
 pub struct Mesh {
-    points: HashMap<PointId, Point>,
-    segments: HashMap<SegmentId, Segment>,
+    points: Vec<(usize, Point)>,
+    segments: Vec<(usize, Segment, usize)>,
     next_id: NextId,
 }
 
@@ -22,36 +19,26 @@ pub enum Segment {
 impl Mesh {
     pub fn empty() -> Self {
         Self {
-            points: HashMap::new(),
-            segments: HashMap::new(),
+            points: Vec::new(),
+            segments: Vec::new(),
             next_id: NextId::new(),
         }
     }
 
-    pub fn append_bezpath(&mut self, bezpath: BezPath) {
-        let mut last_points_id = (None, None);
+    pub fn append_point(&mut self, point: Point) -> usize {
+        let id = self.next_id.next();
+        self.points.push((id, point));
+        id
+    }
 
-        for elm in bezpath.elements() {
-            if *elm == PathEl::ClosePath {
-            } else {
-                let next_id = self.next_id.next();
-                self.points.insert(next_id, elm.end_point().unwrap());
-                last_points_id.0 = last_points_id.1;
-                last_points_id.1 = Some(next_id);
-
-                match elm {
-                    PathEl::QuadTo(point, _) => {
-                        let segment_id = (last_points_id.0.unwrap(), last_points_id.1.unwrap());
-                        self.segments.insert(segment_id, Segment::Quad(*point));
-                    }
-                    PathEl::CurveTo(point, point1, _) => {
-                        let segment_id = (last_points_id.0.unwrap(), last_points_id.1.unwrap());
-                        self.segments
-                            .insert(segment_id, Segment::Cubic(*point, *point1));
-                    }
-                    _ => {}
-                }
-            }
+    pub fn append_segment(&mut self, start: usize, end: usize, segment: Segment) -> Option<usize> {
+        if self.points.iter().find(|(id, _)| *id == start).is_none()
+            || self.points.iter().find(|(id, _)| *id == start).is_none()
+        {
+            return None;
         }
+        let id = self.next_id.next();
+        self.segments.push((start, segment, end));
+        Some(id)
     }
 }
